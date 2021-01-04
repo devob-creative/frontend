d913fd197d82dd983cd3a59599767b749894b70a
eleviven <firuz.haciyeff@gmail.com>
2021-01-04 17:39:09 +0400

    Text types created

---
+++ src/types/text.type.js
@@ -0,0 +1,10 @@
+export const fontTypes = {
+  primary: "font-primary",
+  secondary: "font-secondary",
+};
+
+export const textTransformTypes = {
+  uppercase: "text-uppercase",
+  capitalize: "text-capitalize",
+  lowercase: "text-lowercase",
+};
