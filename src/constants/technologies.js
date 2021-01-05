30e3bd410b6e6f20921bba03deb7d5c6e4cb4fd4
eleviven <firuz.haciyeff@gmail.com>
2021-01-05 21:02:33 +0400

    Technologies constant created

---
+++ src/constants/technologies.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    title: "React",
+    icon: "react",
+  },
+  {
+    title: "Vue JS",
+    icon: "vuejs",
+  },
+  {
+    title: "Node",
+    icon: "node-js",
+  },
+  {
+    title: "React Native",
+    icon: "react",
+  },
+  {
+    title: "php",
+    icon: "php",
+  },
+  {
+    title: "JavaScript",
+    icon: "js",
+  },
+  {
+    title: "Mongo DB",
+    icon: "server",
+    type: "regular",
+  },
+  {
+    title: "Sass",
+    icon: "sass",
+  },
+];
