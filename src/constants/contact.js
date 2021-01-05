d13d27ec73dae0651bbd32e1a49cb3d6fc458f2a
eleviven <firuz.haciyeff@gmail.com>
2021-01-05 14:53:24 +0400

    Contact informations constant created

---
+++ src/constants/contact.js
@@ -0,0 +1,12 @@
+export default {
+  phone: {
+    title: "Telefon",
+    value: "+9940514731423",
+    icon: "phone",
+  },
+  email: {
+    title: "Email",
+    value: "hello@devob.az",
+    icon: "at",
+  },
+};
