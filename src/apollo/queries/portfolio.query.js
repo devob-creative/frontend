d4257d01ea5f1c77b112ffcf89f2d33a0928f451
eleviven <firuz.haciyeff@gmail.com>
2021-01-12 02:26:51 +0400

    Bugs fixed

--- src/apollo/queries/portfolio.query.js
+++ src/apollo/queries/portfolio.query.js
@@ -7,6 +7,7 @@ export const GET_PORTFOLIOS = gql`
       title
       description
       keywords
+      slug
       content
       image {
         url
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Layout, Head, Portfolio, Paragraph, Container } from "../components";
 import { Services, CarouselContainer } from "../containers";
 import { usePortfolios } from "../apollo/actions/portfolio.action";
@@ -14,12 +15,15 @@ function Home() {
       <Container>
         <Portfolio.Wrapper className="mb-5">
           {portfolios?.map((item) => (
-            <Portfolio.Item
-              key={item.id}
-              title={item.title}
-              date={item.created_at}
-              image={item.cover?.url || item.image[0].url}
-            />
+            <Link key={item.id} href={`/projects/${item.slug}`}>
+              <a>
+                <Portfolio.Item
+                  title={item.title}
+                  date={item.created_at}
+                  image={item.cover?.url || item.image[0].url}
+                />
+              </a>
+            </Link>
           ))}
         </Portfolio.Wrapper>
       </Container>
