7b1007e6345375e1812841b168d1519b5df37e78
eleviven <firuz.haciyeff@gmail.com>
2021-01-11 22:28:43 +0400

    Loop datas

--- src/containers/carousel.js
+++ src/containers/carousel.js
@@ -1,16 +1,21 @@
 import { Carousel } from "../components";
 import { SwiperSlide } from "swiper/react";
+import { useSlides } from "../apollo/actions/slide.action";
 
 export default function CarouselContainer({ ...props }) {
+  const { data: { slides } = {}, loading } = useSlides();
   return (
     <Carousel.Wrapper {...props}>
       <SwiperSlide>
-        <Carousel.Item
-          title="We're Creative"
-          image="/images/slide.jpg"
-          actionTitle="Get Started"
-          actionUrl="#"
-        />
+        {slides?.map((item) => (
+          <Carousel.Item
+            key={item.id}
+            title={item.title}
+            image={item.image.url}
+            actionTitle={item.action}
+            actionUrl={item.redirect}
+          />
+        ))}
       </SwiperSlide>
     </Carousel.Wrapper>
   );
