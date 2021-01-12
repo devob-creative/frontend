e5bd2b545d46f476879203627b4708a43d46da81
eleviven <firuz.haciyeff@gmail.com>
2021-01-12 23:57:30 +0400

    Get single portfolio query created

--- src/apollo/queries/portfolio.query.js
+++ src/apollo/queries/portfolio.query.js
@@ -1,29 +1,46 @@
 import { gql } from "@apollo/client";
 
+const PORTFOLIO_FRAGMENT = gql`
+  fragment PortfolioFragment on Portfolio {
+    id
+    title
+    slug
+    image {
+      id
+      url
+    }
+    cover {
+      url
+    }
+    categories {
+      name
+      slug
+    }
+    created_at
+  }
+`;
+
 export const GET_PORTFOLIOS = gql`
   query($where: JSON) {
     portfolios(sort: "created_at:desc", where: $where) {
-      id
-      title
+      ...PortfolioFragment
+    }
+  }
+  ${PORTFOLIO_FRAGMENT}
+`;
+
+export const GET_PORTFOLIO = gql`
+  query($slug: String!) {
+    portfolioBySlug(slug: $slug) {
+      ...PortfolioFragment
       description
       keywords
-      slug
       content
-      image {
-        url
-      }
-      cover {
-        url
-      }
-      categories {
-        name
-        slug
-      }
       colors {
         background
         text
       }
-      created_at
     }
   }
+  ${PORTFOLIO_FRAGMENT}
 `;
