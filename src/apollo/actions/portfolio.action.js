4f5cdc27fc4317cd82a043e837d249c09083b186
eleviven <firuz.haciyeff@gmail.com>
2021-01-12 23:58:00 +0400

    Get single portfolio action add

--- src/apollo/actions/portfolio.action.js
+++ src/apollo/actions/portfolio.action.js
@@ -1,4 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { GET_PORTFOLIOS } from "../queries/portfolio.query";
+import { GET_PORTFOLIOS, GET_PORTFOLIO } from "../queries/portfolio.query";
 
 export const usePortfolios = (options) => useQuery(GET_PORTFOLIOS, options);
+export const usePortfolio = (options) => useQuery(GET_PORTFOLIO, options);
