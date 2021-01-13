779a5e9f34249a69a15af4d813c770e68e2313ea
eleviven <firuz.haciyeff@gmail.com>
2021-01-14 02:59:41 +0400

    Add carousel skeleton

--- src/containers/carousel.js
+++ src/containers/carousel.js
@@ -1,22 +1,29 @@
-import { Carousel } from "../components";
+import { Fragment } from "react";
+import { Carousel, Skeleton } from "../components";
 import { SwiperSlide } from "swiper/react";
 import { useSlides } from "../apollo/actions/slide.action";
 
 export default function CarouselContainer({ ...props }) {
   const { data: { slides } = {}, loading } = useSlides();
   return (
-    <Carousel.Wrapper {...props}>
-      <SwiperSlide>
-        {slides?.map((item) => (
-          <Carousel.Item
-            key={item.id}
-            title={item.title}
-            image={item.image.url}
-            actionTitle={item.action}
-            actionUrl={item.redirect}
-          />
-        ))}
-      </SwiperSlide>
-    </Carousel.Wrapper>
+    <Fragment>
+      {loading ? (
+        <Skeleton.CarouselSkeleton />
+      ) : (
+        <Carousel.Wrapper {...props}>
+          <SwiperSlide>
+            {slides?.map((item) => (
+              <Carousel.Item
+                key={item.id}
+                title={item.title}
+                image={item.image.url}
+                actionTitle={item.action}
+                actionUrl={item.redirect}
+              />
+            ))}
+          </SwiperSlide>
+        </Carousel.Wrapper>
+      )}
+    </Fragment>
   );
 }
