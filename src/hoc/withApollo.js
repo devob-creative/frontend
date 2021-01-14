3ae8d07330de5d40a2cfb5e07ca1fefecfb29cbb
eleviven <firuz.haciyeff@gmail.com>
2021-01-14 15:37:40 +0400

    Modified

--- src/hoc/withApollo.js
+++ src/hoc/withApollo.js
@@ -10,11 +10,9 @@ import { API_URI } from "../constants";
 
 export default withApollo(
   ({ initialState, headers }) => {
-    const isBrowser = typeof window !== undefined;
     return new ApolloClient({
       headers,
       credentials: "same-origin",
-      ssrMode: !isBrowser,
       uri: `${API_URI}/graphql`,
       cache: new InMemoryCache({
         typePolicies: {
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
+import App from "next/app";
+import { getDataFromTree } from "@apollo/client/react/ssr";
+import withApollo from "../hoc/withApollo";
 import "../styles/global.scss";
 import "swiper/swiper.scss";
-import withApollo from "../hoc/withApollo";
-import { getDataFromTree } from "@apollo/client/react/ssr";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
 
 export default withApollo(MyApp, { getDataFromTree });
